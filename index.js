2bb60b8f16b7806d37d3028cfe018300c33597f4
Samuel Silva <samuel-rodrigues-silva>
2021-04-14 08:15:04 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 8080;
+const router  = require('./marvel_api_routes');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/marvel',router);
+
+app.listen(port, ()=>{
+    console.log('BACKEND RUNNING ON PORT' + port);
+});
