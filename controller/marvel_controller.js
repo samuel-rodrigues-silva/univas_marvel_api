3ca569a2cff654d287f6313c09fe8a630281453e
Samuel Silva <samuel-rodrigues-silva>
2021-04-24 14:28:39 -0300

    marvel api

--- controller/marvel_controller.js
+++ controller/marvel_controller.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
-const md5 = require('md5');
-const MarvelApiHost = 'http://gateway.marvel.com/v1/public/characters';
+const http = require('http');
+const { Md5 } = require('ts-md5/dist/md5');
+const MarvelApiHost = 'https://gateway.marvel.com/v1/public/characters';
 const apikey = '6c4ed2deff05cc8ecbcf371162944db7';
 var x = '?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150';
 
 const listAll = (req, res) => {
     let ts = new Date.now();
-    let hash = md5(ts);
-    let request = axios.get(`${MarvelApiHost}?ts=${ts}&apikey=${apikey}&hash=${hash}`).then(response => {
+    let hash = Md5.hashStr(ts);
+    let request = http.get(`${MarvelApiHost}?ts=${ts}&apikey=${apikey}&hash=${hash}`).then(response => {
         res.send(response);
         res.send(request);
     });
